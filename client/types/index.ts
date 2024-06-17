530ed98a585d07580d9d662c5e6a2c32a01d2d13
Hamzah Mohamed <hamzahmo2002@gmail.com>
2024-06-17 15:43:49 +0100

    client: removing time parsing from prayer times

--- client/types/index.ts
+++ client/types/index.ts
@@ -4,16 +4,16 @@ export const PrayerTimesSchema = z.object({
     Day: z.string(),
     DayOfIslamicMonth: z.number(),
     DayOfMonth: z.number(),
-    FajrStart: z.string().time(),
-    FajrJamaah: z.string().time(),
-    Sunrise: z.string().time(),
-    DhuhrStart: z.string().time(),
-    DhuhrJamaah: z.string().time(),
-    AsrStart: z.string().time(),
-    AsrJamaah: z.string().time(),
-    MaghribJamaah: z.string().time(),
-    IshaStart: z.string().time(),
-    IshaJamaah: z.string().time()
+    FajrStart: z.string(),
+    FajrJamaah: z.string(),
+    Sunrise: z.string(),
+    DhuhrStart: z.string(),
+    DhuhrJamaah: z.string(),
+    AsrStart: z.string(),
+    AsrJamaah: z.string(),
+    MaghribJamaah: z.string(),
+    IshaStart: z.string(),
+    IshaJamaah: z.string()
 }).array();
 
 export type PrayerTimes = z.infer<typeof PrayerTimesSchema>;
